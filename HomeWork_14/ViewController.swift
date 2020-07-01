cf69d2e54c0105073a9e64613d3818f18cfe71b8
Vonckad <Vonkad@yandex.ru>
2020-07-01 16:51:05 +0300

    first commit

---
+++ HomeWork_14/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  HomeWork_14
+//
+//  Created by Vlad Ralovich on 6/30/20.
+//  Copyright © 2020 Vonkad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+
+}
+
